625360ebd3292557e2a0012787d17b3621219904
jeongtae Lee <sms3025@ajou.ac.kr>
2025-10-30 16:06:35 +0900

    feat: add Lotto class logic
    logic list
    -validate count => if it has 6 number
    -validate duplication
    -validate out of range => check range in 1 to 45
    -sort in ascending order
    
    Initally, I tried to sort it at the output,
    but I think it's more appropriate to have the
    logic to sort it inside the lotto.

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,20 +1,60 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import lotto.error.ErrorMessage;
 
 public class Lotto {
+    private static final Integer LOTTO_NUMBERS_COUNT = 6;
+    private static final Integer START_LOTTO_RANGE = 1;
+    private static final Integer END_LOTTO_RANGE = 45;
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        List<Integer> copyNumbers = new ArrayList<>(numbers);
+        Collections.sort(copyNumbers);
+        this.numbers = copyNumbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        validateCount(numbers);
+        validateDuplication(numbers);
+        validateRange(numbers);
+    }
+
+    private void validateCount(List<Integer> numbers) {
+        if (isCorrectCount(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_SIX_LOTTO_NUMBERS.getErrorMessage());
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateDuplication(List<Integer> numbers) {
+        if (isDuplicated(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.DUPLICATED_LOTTO_NUMBERS.getErrorMessage());
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        if (isOutOfRange(numbers)) {
+            throw new IllegalArgumentException(ErrorMessage.NOT_RANGED_LOTTO_NUMBERS.getErrorMessage());
+        }
+    }
+
+    private boolean isDuplicated(List<Integer> numbers) {
+        return numbers.size() != numbers.stream().distinct().count();
+    }
+
+    private boolean isOutOfRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch(number -> number < START_LOTTO_RANGE || number > END_LOTTO_RANGE);
+    }
+
+    private boolean isCorrectCount(List<Integer> numbers) {
+        return numbers.size() != LOTTO_NUMBERS_COUNT;
+    }
 }
