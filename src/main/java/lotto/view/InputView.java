7ba689710987f9f569a7f130bcc0b7a3a75124ce
jeongtae Lee <sms3025@ajou.ac.kr>
2025-10-31 16:55:42 +0900

    feat: add input logic
    input logic list
    -lotto price, winner numbers, bonus number

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,4 +1,27 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String LOTTO_PRICE_INPUT_MESSAGE = "구입금액을 입력해 주세요. (최대 10만원 까지, 1000원 단위로 구매 가능합니다.)";
+    private static final String WINNER_NUMBERS_INPUT_MESSAGE = "당첨 번호를 입력해 주세요. (쉼표(,)로 구분합니다.)";
+    private static final String BONUS_NUMBER_INPUT_MESSAGE = "보너스 번호를 입력해 주세요.";
+
+    public InputView() {
+    }
+
+    public String inputLottoPrice() {
+        OutputView.printMessage(LOTTO_PRICE_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputWinnerNumbers() {
+        OutputView.printMessage(WINNER_NUMBERS_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        OutputView.printMessage(BONUS_NUMBER_INPUT_MESSAGE);
+        return Console.readLine();
+    }
 }
--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,10 @@
 package lotto.view;
 
 public class OutputView {
+    public OutputView() {
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
 }
